ca7aff0dcd198f606dd86d67eb685e22529ce449
Luis Velasco <luisvelascom17@gmail.com>
2026-01-20 13:39:10 -0400

    Permisos al superuser para entrar a Dashboard

--- modelNewApp/decorators.py
+++ modelNewApp/decorators.py
@@ -1,13 +1,21 @@
 from django.shortcuts import redirect
 from django.contrib import messages
+from functools import wraps
 
 def solo_admin(view_func):
+    @wraps(view_func)
     def wrapper(request, *args, **kwargs):
         user = request.user
 
+        # 1️⃣ No autenticado
         if not user.is_authenticated:
             return redirect("modelNewApp:login")
 
+        # 2️⃣ Superuser SIEMPRE puede entrar
+        if user.is_superuser:
+            return view_func(request, *args, **kwargs)
+
+        # 3️⃣ Usuario normal → validar estructura Persona → Empleado → Rol
         if not hasattr(user, "persona"):
             messages.error(request, "No tienes permisos para acceder.")
             return redirect("modelNewApp:control")
